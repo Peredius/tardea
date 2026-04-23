0d9d3f7b0243f43ff50bbd86acd3adc7fb067c29
Peredius <davidperedagarate@gmail.com>
2026-04-24 00:01:12 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
       <div className="container-page flex h-16 items-center justify-between">
         <Link href="/" className="flex items-center">
           <Image
-            src="/logo-tardea.png"
+            src="/logotardea.png"
             alt="TARDEA"
             width={140}
             height={40}
