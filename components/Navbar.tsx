84d1ecc12428dbe8a7a1f7e37915eaac211ff642
Peredius <davidperedagarate@gmail.com>
2026-04-23 22:49:00 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ export function Navbar() {
             alt="TARDEA"
             width={140}
             height={40}
-            className="h-8 w-auto"
+            className="h-12 w-auto"
           />
         </Link>
 
