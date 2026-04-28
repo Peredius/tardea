27b815a8b671331a7fdee35825fb6118df3357e0
Peredius <davidperedagarate@gmail.com>
2026-04-28 18:04:50 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vligrpkqsuscppgnawon.supabase.co'
+const supabaseUrl = 'https://vligprkqsuscppgnawon.supabase.co'
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
