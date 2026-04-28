86690808bd44f299753095fa234b5258effc9050
Peredius <davidperedagarate@gmail.com>
2026-04-28 19:51:46 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -37,11 +37,12 @@ const [perks, setPerks] = useState('');
     let imageUrl = '';
 
 if (cover && typeof cover !== 'string') {
-  const fileName = `${Date.now()}-${cover.name}`;
+  const file = cover as File;
+  const fileName = `${Date.now()}-${file.name}`;
 
   const { data, error: uploadError } = await supabase.storage
     .from('events')
-    .upload(fileName, cover);
+    .upload(fileName, file);
 
   if (uploadError) {
     console.error(uploadError);
