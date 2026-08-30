10a2cfef96abcc1b5fb24525677e9f60c95e3920
Peredius <davidperedagarate@gmail.com>
2026-08-30 12:40:50 +0200

    Add more known venue autofill entries

--- lib/venueAutofill.ts
+++ lib/venueAutofill.ts
@@ -14,6 +14,46 @@ const knownVenues: Array<KnownVenueDetails & { aliases: string[] }> = [
     mapsUrl:
       'https://www.google.com/maps/search/?api=1&query=Hip%C3%B3dromo%20de%20la%20Zarzuela%20Avenida%20Padre%20Huidobro%20s%2Fn%20Madrid',
   },
+  {
+    venue: 'Sala But',
+    aliases: ['but', 'sala but', 'ochoymedio', 'ochoymedio club', 'ochoymedio madrid'],
+    address: 'Calle de Barceló, 11, 28004 Madrid',
+    area: 'Centro',
+    mapsUrl:
+      'https://www.google.com/maps/search/?api=1&query=Sala%20But%20Calle%20de%20Barcel%C3%B3%2011%20Madrid',
+  },
+  {
+    venue: 'Florida Park',
+    aliases: ['florida', 'florida park', 'florida park retiro'],
+    address: 'Paseo de la República de Panamá, 1, 28009 Madrid',
+    area: 'Retiro',
+    mapsUrl:
+      'https://www.google.com/maps/search/?api=1&query=Florida%20Park%20Paseo%20de%20la%20Rep%C3%BAblica%20de%20Panam%C3%A1%201%20Madrid',
+  },
+  {
+    venue: 'Teatro Magno',
+    aliases: ['magno', 'teatro magno', 'teatro magno madrid'],
+    address: 'Calle de Cedaceros, 7, 28014 Madrid',
+    area: 'Gran Vía',
+    mapsUrl:
+      'https://www.google.com/maps/search/?api=1&query=Teatro%20Magno%20Calle%20de%20Cedaceros%207%20Madrid',
+  },
+  {
+    venue: 'Shoko Madrid',
+    aliases: ['shoko', 'shoko madrid', 'sala shoko'],
+    address: 'Calle de Toledo, 86, 28005 Madrid',
+    area: 'La Latina',
+    mapsUrl:
+      'https://www.google.com/maps/search/?api=1&query=Shoko%20Madrid%20Calle%20de%20Toledo%2086%20Madrid',
+  },
+  {
+    venue: 'Rubicon',
+    aliases: ['rubicon', 'rubicon madrid', 'rubicón', 'rubicón madrid'],
+    address: 'Calle de José Ortega y Gasset, 71, 28006 Madrid',
+    area: 'Salamanca',
+    mapsUrl:
+      'https://www.google.com/maps/search/?api=1&query=Rubicon%20Calle%20de%20Jos%C3%A9%20Ortega%20y%20Gasset%2071%20Madrid',
+  },
 ]
 
 function normalizeVenueText(value: string) {
